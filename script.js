11c136bf3a2cd42f422adfec1e9bcd8f5896657e
yohannesjho <jhonians@outlook.com>
2022-08-31 03:13:38 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+let weather = {
+    apiKey: "f6012874bc49024d5f1b2161137b48cb",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=%20"
+        +city+
+        "&units=metric&appid="
+        +this.apiKey
+          ) 
+      .then((response)=>response.json())
+      .then((data)=>this.displayWeather(data));
+      },
+      displayWeather:function(data){
+        const {name}=data;
+        const {icon,description}=data.weather[0];
+        const {temp,humidity}=data.main;
+        const {speed}=data.wind;
+       // console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector('.city').innerText="weather in " + name;
+        document.querySelector('.icon ').src="https://openweathermap.org/img/wn/"+icon+'.png';
+        document.querySelector('.description').innerText=description;
+        document.querySelector('.temp').innerText=temp +"°C";
+        document.querySelector('.humidity').innerText="Humidity:" + humidity;
+        document.querySelector('.wind').innerText= "wind speed:"+ speed + "km/hr";
+        document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name +" ')";
+     },
+     search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+    };
+
+    document.querySelector('.search button').addEventListener('click', function() {
+      weather.search();
+    });
+
+    document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+  weather.fetchWeather('addis ababa');
